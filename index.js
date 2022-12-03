d4c899a1ba710cf734dd9dbac7e9966ab6c49c48
Daniel Young <Danyoung91@gmail.com>
2022-12-03 15:46:16 -0500

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+
+  for (let i = 0; i < array.length; i++) {
+    for (j = 0; j < array.length; j++) {
+      if (array[i] + array[j] === target && i != j) {
+        console.log(array[i], array[j]);
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 /* 
@@ -17,15 +27,15 @@ function hasTargetSum(array, target) {
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
+  // console.log("Expecting: true");
+  // console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
-  console.log("");
+  // console.log("");
 
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
+  // console.log("Expecting: true");
+  // console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
 
-  console.log("");
+  // console.log("");
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
